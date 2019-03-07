3cee695fa8e5bab438e0ccb4c374a20986e2bd38
Chris Pritchard <chris.pritchard@edpay.nz>
2019-03-08 12:03:34 +1300

    fixed bug with header and moved to its own function

--- Program.fs
+++ Program.fs
@@ -1,24 +1,30 @@
 ﻿
 open System.IO
 
-[<EntryPoint>]
-let main _ =
-    
+let ``Save an image to disk`` () =
     let fileName = "./out.ppm"
 
     if File.Exists fileName then File.Delete fileName
     use out = File.OpenWrite fileName
 
     let width, height = 512, 512
-    sprintf "P6\n%i %i\n" width height |> Seq.iter (fun c -> out.WriteByte (byte c))
+    // ppm header
+    sprintf "P6\n%i %i\n255\n" width height 
+    |> Seq.iter (fun c -> out.WriteByte (byte c))
+
+    // bytes, in order rgb
     [1..width*height] 
     |> List.collect (fun i ->
         let row = float (i / height)
         let col = float (i % height)
         let red = byte ((255.*col)/float height)
         let green = byte ((255.*row)/float width)
-
         [red;green;0uy])
     |> Seq.iter out.WriteByte
+
+[<EntryPoint>]
+let main _ =
+    
+    ``Save an image to disk`` ()
 
     0
