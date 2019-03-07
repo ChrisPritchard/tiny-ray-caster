ba4c4bce4b4a5bf4d4d25206e7e8842c0e2443fb
Chris Pritchard <chris.pritchard@edpay.nz>
2019-03-08 11:16:49 +1300

    initial implementation

--- Program.fs
+++ Program.fs
@@ -1,8 +1,24 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿
+open System.IO
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    
+    let fileName = "./out.ppm"
+
+    if File.Exists fileName then File.Delete fileName
+    use out = File.OpenWrite fileName
+
+    let width, height = 512, 512
+    sprintf "P6\n%i %i\n" width height |> Seq.iter (fun c -> out.WriteByte (byte c))
+    [1..width*height] 
+    |> List.collect (fun i ->
+        let row = float (i / height)
+        let col = float (i % height)
+        let red = byte ((255.*col)/float height)
+        let green = byte ((255.*row)/float width)
+
+        [red;green;0uy])
+    |> Seq.iter out.WriteByte
+
+    0
