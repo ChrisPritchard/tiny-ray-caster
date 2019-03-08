38a8c703afa2f3f623011e00d9b2385586b80e2e
Chris Pritchard <chris.pritchard@edpay.nz>
2019-03-08 13:32:41 +1300

    Step 3: draw the player

--- Program.fs
+++ Program.fs
@@ -19,9 +19,16 @@ let map = """
 0              0
 0002222222200000
 """
+let mapArray = map.Trim([|'\r';'\n'|]).Split("\r\n")
+let mapw, maph = mapArray.[0].Length, mapArray.Length
+
+let mapDim array = Array2D.length1 array, Array2D.length2 array
+let tileDim array = 
+    let width, height = mapDim array
+    width / mapw, height / maph
 
 let gradient array =
-    let width, height = Array2D.length1 array, Array2D.length2 array
+    let width, height = mapDim array
     for y = 0 to height - 1 do
         for x = 0 to width - 1 do
             let red = (255.*float y)/float height
@@ -35,13 +42,16 @@ let drawRect x y w h v (array: 'a[,]) =
             array.[x, y] <- v
 
 let drawMap array =
-    let width, height = Array2D.length1 array, Array2D.length2 array
-    let map = map.Trim([|'\r';'\n'|]).Split("\r\n")
-    let tw, th = width / map.[0].Length, height / map.Length
-    for y = 0 to map.Length - 1 do
-        for x = 0 to map.[0].Length - 1 do
-            if map.[y].[x] <> ' ' then
+    let tw, th = tileDim array
+    for y = 0 to maph - 1 do
+        for x = 0 to mapw - 1 do
+            if mapArray.[y].[x] <> ' ' then
                 drawRect (x * tw) (y * th) tw th (0uy, 255uy, 255uy) array
+    array
+
+let drawPlayer px py array =
+    let tw, th = tileDim array
+    drawRect (int (px * float tw)) (int (py * float th)) 5 5 (255uy, 255uy, 255uy) array
     array
 
 let dropPPM fileName array =
@@ -60,9 +70,12 @@ let dropPPM fileName array =
 [<EntryPoint>]
 let main _ =
     
+    let px, py = 3.456, 2.345
+
     Array2D.create 512 512 (0uy, 0uy, 0uy)
     |> gradient
     |> drawMap
+    |> drawPlayer px py
     |> dropPPM "./out.ppm"
 
     0
