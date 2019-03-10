5c1c5b4a9e42cf9677c3fc9c998a720cfdf7be5a
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-03-11 11:02:29 +1300

    slight refactor. moved map into an external file

--- Program.fs
+++ Program.fs
@@ -1,33 +1,18 @@
 ﻿
 open SDL
 open System
+open System.IO
 open System.Runtime.InteropServices
 open System.Drawing
 
-let map = 
-    [|
-        "0000222222220000"
-        "1              0"
-        "1      11111   0"
-        "1     0        0"
-        "0     0  1110000"
-        "0     3        0"
-        "0   10000      0"
-        "0   0   11100  0"
-        "0   0   0      0"
-        "0   0   1  00000"
-        "0       1      0"
-        "2       1      0"
-        "0       0      0"
-        "0 0000000      0"
-        "0              0"
-        "0002222222200000"
-    |] |> array2D
+let map = File.ReadAllLines "map.txt" |> array2D
+let isOpen (x: float) (y: float) = map.[int x, int y] = ' '
 
-let arrayw, arrayh = 1024, 512
+let viewWidth, viewHeight = 1024, 512
 let mapw, maph = map.GetLength(0), map.GetLength(1)
-let tilew, tileh = 32, 32
+let tilew, tileh = viewWidth/2/mapw, viewHeight/maph
 let fov = Math.PI/3.
+let turnSpeed, walkSpeed = 0.1, 0.1
 
 let asUint32 (r, g, b) = BitConverter.ToUInt32 (ReadOnlySpan [|b; g; r; 255uy|])
 
@@ -44,14 +29,14 @@ let wallRows () =
 
 let drawRect x y w h v array =
     for dy = y to y + h - 1 do
-        let pos = (dy * arrayw) + x
+        let pos = (dy * viewWidth) + x
         Array.fill array pos w v
 
 let drawMap wallRows array =
-    drawRect 0 0 (arrayw/2) arrayh white array
+    drawRect 0 0 (viewWidth/2) viewHeight white array
     for y = 0 to maph - 1 do
         for x = 0 to mapw - 1 do
-            if map.[x, y] <> ' ' then
+            if not (isOpen (float x) (float y)) then
                 let wallType = int map.[x, y] - int '0'
                 let wallColour = Array.get wallRows (wallType*64) |> Array.head
                 drawRect (x * tilew) (y * tileh) tilew tileh wallColour array
@@ -68,32 +53,32 @@ let drawRay px py pa array =
         | None ->
             let cx =  px + c * cpa
             let cy = py + c * spa
-            if map.[int cx, int cy] <> ' ' then 
+            if not (isOpen cx cy) then 
                 let fcx, fcy = fraction cx, fraction cy
                 let ratio = if fcx > 0.01 && fcx < 0.99 then fcx else fcy
                 Some (c, int map.[int cx, int cy] - int '0', ratio)
             else
                 let pixelx, pixely = int (cx * float tilew), int (cy * float tileh)
-                Array.set array (pixely * arrayw + pixelx) black
+                Array.set array (pixely * viewWidth + pixelx) black
                 None)
 
 let drawView px py pa wallRows array =
-    drawRect (arrayw/2) 0 (arrayw/2) (arrayh/2) ceilingColour array
-    drawRect (arrayw/2) (arrayh/2) (arrayw/2) (arrayh/2) floorColour array
-    let da = fov / (float arrayw/2.)
-    [0..(arrayw/2)-1] |> List.iter (fun i ->
+    drawRect (viewWidth/2) 0 (viewWidth/2) (viewHeight/2) ceilingColour array
+    drawRect (viewWidth/2) (viewHeight/2) (viewWidth/2) (viewHeight/2) floorColour array
+    let da = fov / (float viewWidth/2.)
+    [0..(viewWidth/2)-1] |> List.iter (fun i ->
         let angle = pa-(fov/2.) + (da*float i)
         match drawRay px py angle array with
         | None -> ()
         | Some (stopPoint, wallType, wallCol) ->
             let viewPlaneDist = stopPoint * cos (angle - pa) // I understand what this does, and why. But not how (not a math guy :)
-            let columnHeight = int (float arrayh / viewPlaneDist)
+            let columnHeight = int (float viewHeight / viewPlaneDist)
             let wallRow = Array.get wallRows (wallType * 64 + int (wallCol * 64.))
             let wy = 64. / float columnHeight
-            let x = (arrayw / 2) + i
-            let y = (arrayh - columnHeight) / 2
+            let x = (viewWidth / 2) + i
+            let y = (viewHeight - columnHeight) / 2
             for dy = y to y + columnHeight - 1 do
-                let pos = dy * arrayw + x
+                let pos = dy * viewWidth + x
                 let pix = int (wy * (float dy - float y))
                 array.[pos] <- Array.get wallRow pix)
             
@@ -104,10 +89,10 @@ let main _ =
 
     let mutable window, renderer = IntPtr.Zero, IntPtr.Zero
     let windowFlags = SDL_WindowFlags.SDL_WINDOW_SHOWN ||| SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS
-    SDL_CreateWindowAndRenderer(arrayw, arrayh, windowFlags, &window, &renderer) |> ignore
-    let texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, arrayw, arrayh)
+    SDL_CreateWindowAndRenderer(viewWidth, viewHeight, windowFlags, &window, &renderer) |> ignore
+    let texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, viewWidth, viewHeight)
 
-    let frameBuffer = Array.create (arrayw * arrayh) black
+    let frameBuffer = Array.create (viewWidth * viewHeight) black
     let bufferPtr = IntPtr ((Marshal.UnsafeAddrOfPinnedArrayElement (frameBuffer, 0)).ToPointer ())
     let mutable keyEvent = Unchecked.defaultof<SDL_KeyboardEvent>
 
@@ -117,7 +102,7 @@ let main _ =
         drawMap wallRows frameBuffer
         drawView px py pa wallRows frameBuffer
 
-        SDL_UpdateTexture(texture, IntPtr.Zero, bufferPtr, arrayw * 4) |> ignore
+        SDL_UpdateTexture(texture, IntPtr.Zero, bufferPtr, viewWidth * 4) |> ignore
         SDL_RenderClear(renderer) |> ignore
         SDL_RenderCopy(renderer, texture, IntPtr.Zero, IntPtr.Zero) |> ignore
         SDL_RenderPresent(renderer) |> ignore
@@ -127,6 +112,13 @@ let main _ =
         else if keyEvent.keysym.sym = SDLK_ESCAPE then 
             () // quit the game by executing the loop
         else
+            let pa =
+                if keyEvent.keysym.sym = uint32 'a' then
+                    pa - turnSpeed
+                elif keyEvent.keysym.sym = uint32 'd' then
+                    pa + turnSpeed
+                else
+                    pa
             drawLoop px py pa    
 
     let px, py, pa = 3.456, 2.345, 0.
